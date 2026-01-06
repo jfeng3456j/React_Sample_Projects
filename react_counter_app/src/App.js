ad85a464f94052b4376dfaaa6ff0303b9fe113c8
jfeng3456j <jfeng3456j@gmail.com>
2026-01-05 22:49:23 -0800

    add useState and useEffect to track counter state changes

--- react_counter_app/src/App.js
+++ react_counter_app/src/App.js
@@ -1,16 +1,37 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    return () => {
+      console.log("clean up");
+    };
+  }, [counter]);
+
+  const inc = () => {
+    setCounter(counter + 1);
+  };
+
+  const dec = () => {
+    if (counter <= 0) {
+      setCounter(0);
+    } else {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Simple React Counter App </p>
+        <h3>Simple React Counter App </h3>
       </header>
 
       <div className="container">
-        <p>count: </p>
-        <button> Increment </button>
-        <button> Decrement </button>
+        <h3>count: {counter}</h3>
+        <button onClick={inc}> Increment </button>
+        <button onClick={dec}> Decrement </button>
       </div>
     </div>
   );
